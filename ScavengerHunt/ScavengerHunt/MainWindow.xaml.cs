240ea82c7f6be99a85ca11b930d63282151c8521
Brandon <bl.fox@live.com>
2015-08-19 11:17:33 -0700

    Created a basic ControlTemplate and edited a style to match the Google Material Design Buttons. Added the ability to open file dialogs to select picture files. Added the ability to create new rows for each scavenger hunt item.

--- ScavengerHunt/ScavengerHunt/MainWindow.xaml.cs
+++ ScavengerHunt/ScavengerHunt/MainWindow.xaml.cs
@@ -20,9 +20,73 @@ namespace ScavengerHunt
     /// </summary>
     public partial class MainWindow : Window
     {
+        int num = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void openFile(object sender, RoutedEventArgs e)
+        {
+            
+            var dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Filles (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+            }
+
+
+
+        }
+
+        private void addRow(object sender, RoutedEventArgs e)
+        {
+            //increase number of rows
+            num++;
+
+            //create button dynamically
+            var newBtn = new Button();
+
+            //Text on button
+            newBtn.Content = "OPEN FILE";
+
+            //Properties of button
+            newBtn.Width = 100;
+            newBtn.Height = 35;
+            newBtn.Style = (Style)this.Resources["GoogleGreyButton"];
+
+            //Location on Screen
+            Canvas.SetTop(newBtn, 50d * (num - 1));
+            Canvas.SetLeft(newBtn, 370d);
+
+            newBtn.Click += openFile;
+
+            //Add to Screen
+            LayoutRoot.Children.Add(newBtn);
+
+            //create Label dynamically
+            var dynamicLabel = new Label();
+
+            //Text on Label
+            dynamicLabel.Content = "Row " + num;
+
+            //Properties of label
+            dynamicLabel.Width = 100;
+            dynamicLabel.Height = 30;
+            dynamicLabel.Foreground = new SolidColorBrush(Colors.Black);
+
+            //Location on Screen
+            Canvas.SetTop(dynamicLabel, 50d * (num - 1));
+            Canvas.SetLeft(dynamicLabel, 250d);
+
+            //Add to Screen
+            LayoutRoot.Children.Add(dynamicLabel);
         }
     }
 }
