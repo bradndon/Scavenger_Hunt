35295f38a4ddf79a00e25ac2de065901b425c320
Brandon <bl.fox@live.com>
2015-08-25 00:35:33 -0700

    Added logic for creating pdfs and a way to interface between the two. Added the answer open file button Started to create a custom text box style to match Material Design Standards

--- ScavengerHunt/ScavengerHunt/MainWindow.xaml.cs
+++ ScavengerHunt/ScavengerHunt/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace ScavengerHunt
 {
@@ -20,12 +23,15 @@ namespace ScavengerHunt
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> imgList;
+        List<Button> btnList;
+        List<Label> lblList;
         int num = 0;
         public MainWindow()
         {
             InitializeComponent();
+           
             Application.Current.Resources["Color400"] = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF7043"));
-
             Application.Current.Resources["Color500"] = (SolidColorBrush)(new BrushConverter().ConvertFrom("#FF5722"));
             Application.Current.Resources["Color600"] = (SolidColorBrush)(new BrushConverter().ConvertFrom("#F4511E"));
             Application.Current.Resources["Color700"] = (SolidColorBrush)(new BrushConverter().ConvertFrom("#E64A19"));
@@ -34,23 +40,29 @@ namespace ScavengerHunt
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             changeSize();
-            
+            btnList = new List<Button>();
+            imgList = new List<string>();
+            lblList = new List<Label>();
         }
 
    
         private void openFile(object sender, RoutedEventArgs e)
         {
-            
+            Trace.WriteLine(btnList.IndexOf((Button)sender));
+
+
             var dlg = new Microsoft.Win32.OpenFileDialog();
 
             dlg.DefaultExt = ".png";
-            dlg.Filter = "JPEG Filles (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+            dlg.Filter = "PNG Files (*.png)|*.png|JPEG Filles (*.jpeg)|*.jpeg|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
 
             Nullable<bool> result = dlg.ShowDialog();
 
             if (result == true)
             {
                 string filename = dlg.FileName;
+                imgList[btnList.IndexOf((Button)sender)] = filename;
+                lblList[btnList.IndexOf((Button)sender)].Content = filename;
             }
 
 
@@ -60,8 +72,10 @@ namespace ScavengerHunt
         private void addRow(object sender, RoutedEventArgs e)
         {
             //increase number of rows
-            //num++;
-
+            num++;
+            imgList.Add(null);
+            imgList.Add(null);
+           
             var newCanvas = new Canvas();
             newCanvas.Width = 940;
             newCanvas.Height = 72;
@@ -69,6 +83,27 @@ namespace ScavengerHunt
 
 
             //create button dynamically
+            var newBtn1 = new Button();
+
+            //Text on button
+            newBtn1.Content = "OPEN FILE";
+
+            //Properties of button
+            newBtn1.Width = 100;
+            newBtn1.Height = 36;
+            newBtn1.Style = (Style)this.Resources["MaterialAccentButton"];
+            
+
+            //Location on Screen
+            Canvas.SetTop(newBtn1, 18d);
+            Canvas.SetLeft(newBtn1, 172d);
+
+            newBtn1.Click += openFile;
+
+            //Add to Screen
+            newCanvas.Children.Add(newBtn1);
+            btnList.Add(newBtn1);
+
             var newBtn = new Button();
 
             //Text on button
@@ -79,20 +114,23 @@ namespace ScavengerHunt
             newBtn.Height = 36;
             newBtn.Style = (Style)this.Resources["MaterialAccentButton"];
 
+
             //Location on Screen
             Canvas.SetTop(newBtn, 18d);
-            Canvas.SetLeft(newBtn, 172d);
+            Canvas.SetLeft(newBtn, 444d);
 
             newBtn.Click += openFile;
 
             //Add to Screen
             newCanvas.Children.Add(newBtn);
+            btnList.Add(newBtn);
+
 
             //create Label dynamically
             var dynamicLabel = new Label();
 
             //Text on Label
-            dynamicLabel.Content = "Question 1 Image Path:";
+            dynamicLabel.Content = "Question " + num + " Image Path:";
 
             //Properties of label
             dynamicLabel.Width = 140;
@@ -105,6 +143,27 @@ namespace ScavengerHunt
 
             //Add to Screen
             newCanvas.Children.Add(dynamicLabel);
+            lblList.Add(dynamicLabel);
+
+            //create Label dynamically
+            var dynamicLabel2 = new Label();
+
+            //Text on Label
+            dynamicLabel2.Content = "Answer " + num + " Image Path:";
+
+            //Properties of label
+            dynamicLabel2.Width = 140;
+            dynamicLabel2.Height = 36;
+            dynamicLabel2.Foreground = new SolidColorBrush(Colors.Black);
+
+            //Location on Screen
+            Canvas.SetTop(dynamicLabel2, 20d);
+            Canvas.SetLeft(dynamicLabel2, 288d);
+
+            //Add to Screen
+            newCanvas.Children.Add(dynamicLabel2);
+            lblList.Add(dynamicLabel2);
+            
         }
 
         private void Size_Changed(object sender, SizeChangedEventArgs e)
@@ -127,9 +186,44 @@ namespace ScavengerHunt
                 appBar1.Width = dWidth;
                 appBar2.Width = dWidth;
                 scroller.Height = dHeight - 79;
+                menu.Height = dHeight - 79;
                 Canvas.SetTop(openButton, dHeight - 54);
                 Canvas.SetLeft(openButton, dWidth - 64);
             }
         }
+
+        private void makeButton_Click(object sender, RoutedEventArgs e)
+        {
+            iTextSharp.text.Image[] questions = new iTextSharp.text.Image[imgList.Count / 2];
+            iTextSharp.text.Image[] answers = new iTextSharp.text.Image[imgList.Count/ 2];
+            int i = 0;
+            for (i = 0; i < btnList.Count() ; i ++)
+            {
+                string s = imgList[i];
+                if (s != null)
+                {
+                    if (i %2 == 0)
+                    {
+                        questions[i/2] = iTextSharp.text.Image.GetInstance(s);
+                        Trace.WriteLine("Question: " + s);
+                    } else
+                    {
+                        answers[i/2] = iTextSharp.text.Image.GetInstance(s);
+                        Trace.WriteLine("Answer: " + s);
+                    }
+                    
+                    
+                } else
+                {
+                    Trace.WriteLine("NOT READY");
+                    break;
+                }
+            }
+            if (i == btnList.Count())
+            {
+                Scavenger pdf = new Scavenger(questions, answers);
+            }
+        }
+        
     }
 }
---
+++ ScavengerHunt/ScavengerHunt/PdfMaker.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace ScavengerHunt
+{
+    public class Scavenger
+    {
+        public Scavenger(Image[] questions, Image[] answers)
+        {
+            Make(questions, answers);
+        }
+
+        static void Make(Image[] questions, Image[] answers)
+        {
+            //VARIABLE DEFINITIONS
+            var doc = new Document(PageSize.LETTER.Rotate());
+            var writer = PdfWriter.GetInstance(doc, new FileStream("E:/foxb4/Documents/Doc1.pdf", FileMode.Create));
+            Image ansImg, queImg; //ansImg top right, queImg bottom center
+            string[] alpha = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            //Image[] questions = new Image[26]; //image array for questions
+            //Image[] answers = new Image[26]; //image array for answers
+
+            //find all the images
+            //for (int i = 1; i <= 26; i++)
+            //{
+            //    questions[i - 1] = Image.GetInstance("E:/Downloads/" + i + ".jpg");
+            //    answers[i - 1] = Image.GetInstance("E:/Downloads/" + i + ".jpg");
+            //}
+
+            //shuffle alphabet array
+            new Random().Shuffle(alpha);
+
+            //open document
+            doc.Open();
+            var cb = writer.DirectContent;
+
+            //main loop, creates pages and follows the template for the layout
+            for (int i = 0; i < answers.Length; i++)
+            {
+                //add a new page
+                doc.NewPage();
+
+                //boxes for refrence REMOVE FOR RELEASE
+                cb.Rectangle(doc.PageSize.Width - 36f - 240f, doc.PageSize.Height - 36f - 200f, 240f, 200f);
+                cb.Rectangle(36f, doc.PageSize.Height - 36f - 200f, 240f, 200f);
+                cb.Rectangle(36f, 36f, 720f, 302f);
+                cb.Stroke();
+
+                //formatting for letter
+                var myText = new Phrase(alpha[i]);
+                myText.Font.Size = 108f;
+                var ct = new ColumnText(cb);
+                ct.SetSimpleColumn(myText, 36f, doc.PageSize.Height - 36f - 100f, 277, 200, 32, Element.ALIGN_CENTER);
+                ct.Go();
+
+                //format image for top right
+                ansImg = answers[i];
+                ansImg.ScaleToFit(240f, 200f);
+                ansImg.SetAbsolutePosition(doc.PageSize.Width - 36f - 240f, doc.PageSize.Height - 36f - 100f - ansImg.ScaledHeight / 2);
+                doc.Add(ansImg);
+
+                //format image for bottom middle
+                queImg = questions[((i + 1) % answers.Length)];
+                queImg.ScaleToFit(720f, 302f);
+                queImg.SetAbsolutePosition(36f, 36f + 151f - queImg.ScaledHeight / 2);
+                doc.Add(queImg);
+            }
+
+            //closethe document
+            doc.Close();
+
+            //open finished document
+            System.Diagnostics.Process.Start("E:/foxb4/Documents/Doc1.pdf");
+        }
+    }
+    //extension for shuffling an array (based on the Fisher-Yates Algorithm)
+    static class RandomExtensions
+    {
+        public static void Shuffle<T>(this Random rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
+
+}
